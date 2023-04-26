8cd43cf66d07588d8c6172f757dd57850d97a6fc
Elesdes <53079743+Elesdes@users.noreply.github.com>
2023-04-26 11:39:13 +0200

    Add app launch for fastapi in main

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import uvicorn
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
@@ -11,9 +12,17 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Oui"}
 
 
 @app.get('/test', response_class=HTMLResponse)
 def test(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",
+                host="127.0.0.1",
+                port=8001,
+                reload=True,
+                log_level="debug")
