fb93a78e791d5d5cc14c185521b6ec6dc70c25cd
Elesdes <53079743+Elesdes@users.noreply.github.com>
2023-04-26 11:15:57 +0200

    Website init

--- api/main.py
+++ api/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get('/test', response_class=HTMLResponse)
+def test(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
